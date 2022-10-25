13d69601a346ca7c56dc5f8bea45ce7df6c2cb76
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-10-25 11:39:23 -0300

    Páginas e rotas - Componente: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
-import { HeaderContainer } from "./styles";
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+
+import logo from '../../assets/Logo.svg'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={logo} alt="" />
       <nav>
-        <a href="">Timer</a>
-        <a href="">History</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
