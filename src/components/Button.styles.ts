817ab1962b0557e3bba5bdd9453ea4e82b298057
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-10-25 10:10:04 -0300

    Commit: Estrutura da aplicação - Configurando temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -7,10 +7,21 @@ interface ButtonContainerProps {
 }
 
 const ButtonVariants = {
-
+   primary: 'purple',
+   secondary: 'orange',
+   danger:'red',
+   success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
    width: 180px;
    height: 60px;
+   
+   background-color: ${props => props.theme.primary}
+
+/* ${props => {
+      return css`
+         background-color: ${ButtonVariants[props.variant]};
+      `
+   }} */
 `
