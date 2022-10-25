c950c8b3dfc79e811e5607cc084138f7598b3cb6
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-10-25 16:32:42 -0300

    Commit: Formulários - Controlled vs Uncontrolled

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,4 +1,5 @@
 import { Play } from 'phosphor-react'
+import { useState } from 'react'
 
 import {
   CountdownContainer,
@@ -11,6 +12,8 @@ import {
 } from './styles'
 
 export function Home() {
+  const [task, setTask] = useState('')
+
   return (
     <HomeContainer>
       <form action="">
@@ -20,6 +23,8 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
           />
 
           <datalist id="task-suggestions">
@@ -48,7 +53,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartContDownButton type="submit">
+        <StartContDownButton disabled={!task} type="submit">
           <Play />
           Começar
         </StartContDownButton>
